0888814c1e71ef47366f5f31e52061499990f446
Yechiel Klein <kleinyechiel@gmail.com>
2023-05-24 00:26:29 -0400

    used state to display dynamic heart size

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,10 +1,11 @@
+import { useState } from 'react';
+
 import alligator from './svg/alligator.svg';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
 import dog from './svg/dog.svg';
 import horse from './svg/horse.svg';
-
 import heart from './svg/heart.svg';
 
 const svgMap = {
@@ -17,10 +18,19 @@ const svgMap = {
 };
 
 function AnimalShow({ type }) {
+	const [heartSize, setHeartSize] = useState(20);
+	const handleClick = () => {
+		if (heartSize >= 200) {
+			return
+		};
+
+		setHeartSize(heartSize + 20);
+	};
+
 	return (
-		<div>
-			<img src={svgMap[type]} alt={type} />
-			<img src={heart} alt="heart" />
+		<div onClick={handleClick}>
+			<img src={svgMap[type]} alt={type} height={200} />
+			<img src={heart} alt="heart" height={heartSize} />
 		</div>
 	);
 };
